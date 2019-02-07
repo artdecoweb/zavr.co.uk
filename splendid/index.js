eafaf59f19c1e6a0fd2a141e146bac7c94789a37
Anton <anton@adc.sh>
2019-02-07 20:22:20 +0300

    add splendid

---
+++ splendid/index.js
@@ -0,0 +1,26 @@
+import { join } from 'path'
+
+const APP_DIR = 'splendid'
+const PAGES_DIR = join(APP_DIR, 'pages')
+const BUILD_DIR = 'docs'
+
+const config = {
+  layout: join(APP_DIR, 'layout/main.html'),
+  appDir: APP_DIR,
+  pagesDir: PAGES_DIR,
+  pre: [
+    {
+      re: /{{ company }}/g,
+      replacement: '[Zavr](https://zavr.co.uk)',
+    },
+  ],
+  postProcess: [
+    {
+      re: /{{ year }}/g,
+      replacement: `${new Date().getFullYear()}`,
+    },
+  ],
+  output: BUILD_DIR,
+}
+
+export default config
