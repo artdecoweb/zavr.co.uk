ca98bd163e3acf1fd2150c6ca7caec2dc6a8f850
Anton <anton@adc.sh>
2019-02-12 18:16:49 +0300

    11 feb

--- splendid/pages/index.js
+++ splendid/pages/index.js
@@ -3,8 +3,18 @@
  */
 export const main = {
   title: 'zavr blog',
-  url: 'index.html',
-  file: 'feb',
+  url: 'index.html', // '11-17-february.html',
+  file: '11-17-february',
   menuUrl: '/',
+  reverse: true,
+}
+
+/**
+ * @type {import('splendid').Page}
+ */
+export const feb4_10 = {
+  title: '4-10 February 2019',
+  url: '4-10-february.html',
+  file: '4-10-february',
   reverse: true,
 }
