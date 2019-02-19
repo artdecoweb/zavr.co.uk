88be7ffc509f0fdc05d1a58db8e723d44c672098
Anton <anton@adc.sh>
2019-02-19 05:39:35 +0300

    add new week

---
+++ pages/18-24-february/index.md
@@ -0,0 +1,6 @@
+## 18-24 February
+
+The Week!
+
+<SectionBreak id="-3" nextId="0" />
+<!-- <SectionBreak nextId="0" /> -->
--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,24 @@
 /**
- * @type {import('splendid').Page}
+ * @type {Object.<string, import('splendid').Page>}
  */
-export const main = {
-  title: 'zavr blog',
-  url: 'index.html', // '11-17-february.html',
-  file: '11-17-february',
-  menuUrl: '/',
-  reverse: true,
-}
-
-/**
- * @type {import('splendid').Page}
- */
-export const feb4_10 = {
-  title: '4-10 February 2019',
-  url: '4-10-february.html',
-  file: '4-10-february',
-  reverse: true,
+export default {
+  feb18_24: {
+    title: 'zavr blog',
+    url: 'index.html', // '18-24-february.html',
+    file: '18-24-february',
+    menuUrl: '/',
+    reverse: true,
+  },
+  feb11_17: {
+    title: '11-17 February 2019',
+    url: '11-17-february.html',
+    file: '11-17-february',
+    reverse: true,
+  },
+  feb4_10: {
+    title: '4-10 February 2019',
+    url: '4-10-february.html',
+    file: '4-10-february',
+    reverse: true,
+  },
 }
