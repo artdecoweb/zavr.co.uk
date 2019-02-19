2ae676956cbfe3633b174ca46ce001f41571e8e4
Anton <anton@adc.sh>
2019-02-19 07:03:18 +0300

    conf

--- splendid.js
+++ splendid.js
@@ -1,26 +1,17 @@
 import { join } from 'path'
 
 const APP_DIR = '.'
-const PAGES_DIR = join(APP_DIR, 'pages')
-const BUILD_DIR = 'docs'
 
 const config = {
   layout: join(APP_DIR, 'layout/main.html'),
   appDir: APP_DIR,
-  pagesDir: PAGES_DIR,
-  pre: [
+  replacements: [
     {
       re: /{{ company }}/g,
       replacement: '[Zavr](https://zavr.co.uk)',
     },
   ],
-  postProcess: [
-    {
-      re: /{{ year }}/g,
-      replacement: `${new Date().getFullYear()}`,
-    },
-  ],
-  output: BUILD_DIR,
+  output: 'docs',
   url: 'https://zavr.co.uk',
 }
 
